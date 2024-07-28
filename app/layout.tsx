5301d9893e26ba921ce7a8aca7f282fd6d3f6f5c
John8414 <jmnguyen276@gmail.com>
2024-07-29 03:16:03 +0700

    update seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,28 +6,27 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Huỳnh Đại Nguyên Portfolio",
+  title: "Huỳnh Đại Nguyên Frontend Developer",
   description: "A Frontend Developer with experience in develop, and maintaining responsive web app to enhance user experience and business efficiency.",
   twitter: {
     card: "summary_large_image",
-    title: "Huỳnh Đại Nguyên Portfolio",
+    title: "Huỳnh Đại Nguyên Frontend Developer",
     description: "A Frontend Developer with experience in develop, and maintaining responsive web app to enhance user experience and business efficiency.",
-    images: "./avatar.jpg",
+    images: "https://nguyenhd.herimaniac.click/avatar.jpg",
     siteId: "https://nguyenhd.herimaniac.click/"
   },
   openGraph: {
     type: "website",
     url: "https://nguyenhd.herimaniac.click/",
-    title: "Huỳnh Đại Nguyên Portfolio",
+    title: "Huỳnh Đại Nguyên Frontend Developer",
     description: "A Frontend Developer with experience in develop, and maintaining responsive web app to enhance user experience and business efficiency.",
     images: [{
       url: "/avatar.jpg",
       width: 1200,
       height: 630,
-      alt: "Huỳnh Đại Nguyên Portfolio"
+      alt: "Huỳnh Đại Nguyên Frontend Developer"
     }]
   }
-
 };
 
 export default function RootLayout({
@@ -38,13 +37,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head>
-        <title>Huỳnh Đại Nguyên Portfolio</title>
+        <title>Huỳnh Đại Nguyên Frontend Developer</title>
         <meta name="description" content="A Frontend Developer with experience in develop, and maintaining responsive web app to enhance user experience and business efficiency." />
         <meta name="keywords" content={"ReactJS, NextJS, Frontend"} />
-        <meta property="og:title" content={"Huỳnh Đại Nguyên Portfolio"} />
+        <meta property="og:title" content={"Huỳnh Đại Nguyên Frontend Developer"} />
         <meta property="og:description" content={"A Frontend Developer with experience in develop, and maintaining responsive web app to enhance user experience and business efficiency."} />
         <meta property="og:url" content={"https://nguyenhd.herimaniac.click/"} />
-        <meta property="og:image" content={"./avatar.jpg"} />
+        <meta property="og:image" content={"https://nguyenhd.herimaniac.click/avatar.jpg"} />
         <link rel="canonical" href={"https://nguyenhd.herimaniac.click/"} />
       </Head>
       <body className={inter.className}>{children}</body>
